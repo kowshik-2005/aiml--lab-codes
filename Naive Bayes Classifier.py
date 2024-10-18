748778803b69624f357580bd0529d11ae576f36f
DAVANPALLY KOWSHIK RAO <kowshikraod2005@gmail.com>
2024-10-18 12:47:41 +0530

    Add files via upload

---
+++ Naive Bayes Classifier.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+data = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.3)
+
+# Naive Bayes Classifier
+model = GaussianNB()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+# Accuracy
+print("Accuracy:", accuracy_score(y_test, y_pred))
